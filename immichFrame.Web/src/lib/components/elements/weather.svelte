b7ff94be99a4597f38ba32ba840339a8dfd654a4
jlaw <jtlawren@gmail.com>
2025-10-12 14:03:14 -0400

    More cosmetic changes to create table

--- immichFrame.Web/src/lib/components/elements/weather.svelte
+++ immichFrame.Web/src/lib/components/elements/weather.svelte
@@ -44,7 +44,7 @@
 
 <div
 	id="weather"
-	class="fixed top-0 left-0 z-10 text-center text-primary
+	class="fixed top-0 left-0 z-10 text-primary
 	{$configStore.style == 'solid' ? 'bg-secondary rounded-tr-2xl' : ''}
 	{$configStore.style == 'transition' ? 'bg-gradient-to-r from-secondary from-0% pr-10' : ''}
 	{$configStore.style == 'blur' ? 'backdrop-blur-lg rounded-tr-2xl' : ''}	
@@ -53,43 +53,63 @@
 	{#if weather}
         <div
             id="weatherinfo"
-            class="text-xl sm:text-xl md:text-2xl lg:text-3xl font-semibold text-shadow-sm weather-info"
+            class="text-xl sm:text-xl md:text-2xl lg:text-3xl font-semibold text-shadow-sm weather-info current-conditions-wrapper"
         >
 			{#if weather.iconId}
-				<img src="{icons[weather.iconId.replace('-','')]}" class="icon-weather" alt="{weather.description}">
+				<img src="{icons[weather.iconId.replace('-','')]}" class="current icon" alt="{weather.description}">
 			{/if}
          
-            <div class="weather-temperature">{weather.temperature?.toFixed(1)}</div>
-            <div class="weather-unit">{weather.unit}</div>
+            <div class="current temperature">{weather.temperature?.toFixed(1)}{weather.unit}</div>
         </div>
-        <div id = "weatherHighLow" class="text-sm sm:text-sm md:text-md lg:text-xl text-shadow-sm">
-            <div class="weather-hilow">H {weather.tempHigh?.toFixed(0)}° / L {weather.tempLow?.toFixed(0)}°</div>
-            <div class="weather-precip">Precip: {weather.precip}%</div>
+        <div id = "weatherHighLow" style="margin-bottom: 10px;" class="text-sm sm:text-sm md:text-md lg:text-xl text-shadow-sm">
+				<span style="color:#F8DD70;">H {weather.tempHigh?.toFixed(0)}°</span> / <span style="color:#6FC4F5;margin-right:15px">L {weather.tempLow?.toFixed(0)}°</span> 
+				<img style = "display: inline-block;"src="{icons['irain']}" alt="An umbrella"/> {((weather?.precip ?? 0)*100).toFixed(0)}% 
         </div>
 
         {#if $configStore.showWeatherDescription}
-            <p id="weatherdesc" class="text-sm sm:text-sm md:text-md lg:text-xl text-shadow-sm">
+            <p id="weatherdesc" class="text-sm sm:text-sm md:text-md lg:text-xl text-shadow-sm"
+			style="max-width:350px; margin: 15px 0;">
                 {weather.description}
             </p>
         {/if}
 
-        {#each weather.hourlyForecast ?? [] as forecast, index}
-			<div class="forecast-item">
-				<p>{forecast.time != null ? new Date(Number(forecast.time) * 1000).toLocaleTimeString(undefined, { hour: 'numeric', hour12: true }) : ''}
-				{forecast.temperature !== undefined ? Math.round(forecast.temperature) : '--'}°
-				Precip: {forecast.precipProbability}%</p>
+		<div class="forecast-container">
+			<div class="header-row">
+				<span class="date-time-header">&nbsp;</span>
+				<span class="weather-icon-header">&nbsp;</span>
+				<span class="temperature-header">&nbsp;</span>
+
+				<span class="precipitation-header">
+					<img class="inline-icon rain" src="{icons['irain']}" alt="Rain"/>
+				</span>
 			</div>
-		{/each}
 
-		{#each weather.dailyForecast ?? [] as forecast, index}
-			<div class="forecast-item">
-				<p>{forecast.time != null ? new Date(Number(forecast.time) * 1000).toLocaleDateString(undefined, { weekday: 'short' }) : ''}
-					H {forecast.temperatureHigh !== undefined ? Math.round(forecast.temperatureHigh) : '--'}°
-					/ L {forecast.temperatureLow !== undefined ? Math.round(forecast.temperatureLow) : '--'}
-				Precip: {forecast.precipProbability}%</p>
+			{#each weather.hourlyForecast ?? [] as forecast, index}
+			<div class="forecast-item hourly">
+				<span class="time">{forecast.time != null ? new Date(Number(forecast.time) * 1000).toLocaleTimeString(undefined, { hour: 'numeric', hour12: true }) : ''}</span>
+				<span class="forecast-icon-container"><img src="{icons[(forecast.icon ?? '').replace('-','')]}" class="forecast-icon" alt="an icon"></span>
+				<span class="temperature small">{forecast.temperature !== undefined ? Math.round(forecast.temperature) : '--'}°</span>
+
+				 <span class="precipitation-container">
+					 <span class="pop">{(Number(forecast?.precipProbability ?? 0) * 100).toFixed(0)}%</span>
+				 </span>
 			</div>
-		{/each}
+			{/each}
 
-
+			{#each weather.dailyForecast ?? [] as forecast, index}
+			<div class="forecast-item hourly">
+				<span class="day-name">{forecast.time != null ? new Date(Number(forecast.time) * 1000).toLocaleDateString(undefined, { weekday: 'short' }) : ''}</span>
+				<span class="forecast-icon-container"><img class="forecast-icon" src="{icons[(forecast.icon ?? '').replace('-','')]}" alt="an icon"/></span>
+				<span class="temperature-container small">
+					<span class="high-temperature" style="color:#F8DD70;">H {forecast.temperatureHigh !== undefined ? Math.round(forecast.temperatureHigh) : '--'}°</span>
+					<span class="temperature-separator dimmed">/</span>
+					<span class="low-temperature" style="color:#6FC4F5;margin-right:15px">L {forecast.temperatureLow !== undefined ? Math.round(forecast.temperatureLow) : '--'}</span>
+				</span>
+				<span class="precipitation-container">
+					<span class="pop">{(Number(forecast?.precipProbability ?? 0) * 100).toFixed(0)}%</span>
+				</span>
+			</div>
+			{/each}
+		</div>
 	{/if}
 </div>
